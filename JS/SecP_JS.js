e9a882b9a022d490e29c4f0bfee408fb6b76b951
AkhmadeevAnton <loonyp2010@gmail.com>
2019-09-24 21:32:37 +0500

    first commit

---
+++ JS/SecP_JS.js
@@ -0,0 +1,95 @@
+
+/*------------- Form-Search  ------------ */
+function openSearch(state) {
+  document.getElementById("sizeButIn").style.display = state;
+}
+
+function closeSearch(state) {
+  document.getElementById("sizeButIn").style.display = state;
+};
+/*------------- Form-Search  ------------*/
+
+
+
+/*------------- Popups --------------*/
+function openForm(state) {
+    document.getElementById("bacbac").style.display = state; 
+    document.getElementById("onregForm").style.display = state;
+  }
+
+function closeForm(state) {
+    document.getElementById("bacbac").style.display = state; 
+    document.getElementById("onregForm").style.display = state;
+};
+/*------------- Popups --------------*/
+
+
+/*------------- Shift Div  ------------*/
+function ggg() {
+  var probDiv = document.getElementById("problem");
+  probDiv.classList.toggle("shift")
+};
+/*------------- Shift Div  ------------*/
+
+
+/* ------------ Popup Lists City -------------*/
+function openListCity(state) {
+    document.getElementById("transparent").style.display = state; 
+    document.getElementById("SpopupList").style.display = state;
+  };
+
+function openListCity_2(state) {
+    document.getElementById("transparent_2").style.display = state; 
+    document.getElementById("SpopupList_2").style.display = state;
+  };
+
+function openListCity_3(state) {
+    document.getElementById("transparent_3").style.display = state; 
+    document.getElementById("SpopupList_3").style.display = state;
+  };
+/* ------------ Popup Lists City -------------*/
+
+
+/*------------- Toogle List Flights  ------------*/
+function ListFlights() {
+var element = document.getElementById("NameFlights");
+    element.classList.toggle("TooLiFli");
+};
+
+function ListFlights_2() {
+var element2 = document.getElementById("NameFlights_2");
+    element2.classList.toggle("TooLiFli");
+};
+
+function ListFlights_3() {
+var element3 = document.getElementById("NameFlights_3");
+    element3.classList.toggle("TooLiFli");
+};
+
+function ListFlights_4() {
+var element4 = document.getElementById("NameFlights_4");
+    element4.classList.toggle("TooLiFli");
+};
+
+function ListFlights_5() {
+var element4 = document.getElementById("NameFlights_5");
+    element4.classList.toggle("TooLiFli");
+};
+
+function ListFlights_6() {
+var element4 = document.getElementById("NameFlights_6");
+    element4.classList.toggle("TooLiFli");
+};
+
+function ListFlights_7() {
+var element4 = document.getElementById("NameFlights_7");
+    element4.classList.toggle("TooLiFli");
+};
+
+function ListFlights_8() {
+var element4 = document.getElementById("NameFlights_8");
+    element4.classList.toggle("TooLiFli_NF_8");
+};
+/*------------- Toogle List Flights  ------------*/
+
+
